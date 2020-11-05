ac2bbd725d5826868214bead84071342c42688b9
Jaak Lin <linjaak@gmail.com>
2020-11-05 15:24:43 +0100

    html to text & upload sample data

--- starter_code.py
+++ starter_code.py
@@ -1,9 +1,15 @@
 import gzip
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+from warcio.archiveiterator import ArchiveIterator
+# https://pypi.org/project/warcio/
+from html_to_text import html_to_text
 
 KEYNAME = "WARC-TREC-ID"
+#KEYNAME = "WARC-Record-ID"
 
 # The goal of this function process the webpage and returns a list of labels -> entity ID
-def find_labels(payload):
+def find_keys(payload):
     if payload == '':
         return
 
@@ -14,39 +20,7 @@ def find_labels(payload):
     for line in payload.splitlines():
         if line.startswith(KEYNAME):
             key = line.split(': ')[1]
-            break
-
-    # Problem 1: The webpage is typically encoded in HTML format.
-    # We should get rid of the HTML tags and retrieve the text. How can we do it?
-
-    # Problem 2: Let's assume that we found a way to retrieve the text from a webpage. How can we recognize the
-    # entities in the text?
-
-    # Problem 3: We now have to disambiguate the entities in the text. For instance, let's assugme that we identified
-    # the entity "Michael Jordan". Which entity in Wikidata is the one that is referred to in the text?
-
-    # To tackle this problem, you have access to two tools that can be useful. The first is a SPARQL engine (Trident)
-    # with a local copy of Wikidata. The file "test_sparql.py" shows how you can execute SPARQL queries to retrieve
-    # valuable knowledge. Please be aware that a SPARQL engine is not the best tool in case you want to lookup for
-    # some strings. For this task, you can use elasticsearch, which is also installed in the docker image.
-    # The file start_elasticsearch_server.sh will start the elasticsearch server while the file
-    # test_elasticsearch_server.py shows how you can query the engine.
-
-    # A simple implementation would be to first query elasticsearch to retrieve all the entities with a label
-    # that is similar to the text found in the web page. Then, you can access the SPARQL engine to retrieve valuable
-    # knowledge that can help you to disambiguate the entity. For instance, if you know that the webpage refers to persons
-    # then you can query the knowledge base to filter out all the entities that are not persons...
-
-    # Obviously, more sophisticated implementations that the one suggested above are more than welcome :-)
-
-
-    # For now, we are cheating. We are going to returthe labels that we stored in sample-labels-cheat.txt
-    # Instead of doing that, you should process the text to identify the entities. Your implementation should return
-    # the discovered disambiguated entities with the same format so that I can check the performance of your program.
-    cheats = dict((line.split('\t', 2) for line in open('data/sample-labels-cheat.txt').read().splitlines()))
-    for label, wikidata_id in cheats.items():
-        if key and (label in payload):
-            yield key, label, wikidata_id
+            return key;
 
 
 def split_records(stream):
@@ -58,16 +32,27 @@ def split_records(stream):
         else:
             payload += line
     yield payload
-
+'''
+def retrieveText(record):
+    if record:
+        soup = BeautifulSoup(record,"html5lib")
+'''
 if __name__ == '__main__':
     import sys
     try:
         _, INPUT = sys.argv
     except Exception as e:
-        print('Usage: python starter-code.py INPUT')
+        print('Usage: python starter-code.py INPUT(war.gz file)')
         sys.exit(0)
+    count = 0
+    with gzip.open(INPUT, 'rb') as stream:
+        for record in ArchiveIterator(stream):
+            if record.rec_type == 'response':
+                    #print(record.rec_headers.get_header(KEYNAME))
+                    # the key_ID storing WebpageID, the text storing the text converted by html
+                    key_ID = record.rec_headers.get_header(KEYNAME)
+                    htmlcontent = record.content_stream().read()
+                    soup = BeautifulSoup(htmlcontent, "html5lib")
+                    text = html_to_text(soup)
 
-    with gzip.open(INPUT, 'rt', errors='ignore') as fo:
-        for record in split_records(fo):
-            for key, label, wikidata_id in find_labels(record):
-                print(key + '\t' + label + '\t' + wikidata_id)
+
---
+++ starter_code_origninal.py
@@ -0,0 +1,73 @@
+import gzip
+
+KEYNAME = "WARC-TREC-ID"
+
+# The goal of this function process the webpage and returns a list of labels -> entity ID
+def find_labels(payload):
+    if payload == '':
+        return
+
+    # The variable payload contains the source code of a webpage and some additional meta-data.
+    # We firt retrieve the ID of the webpage, which is indicated in a line that starts with KEYNAME.
+    # The ID is contained in the variable 'key'
+    key = None
+    for line in payload.splitlines():
+        if line.startswith(KEYNAME):
+            key = line.split(': ')[1]
+            break
+
+    # Problem 1: The webpage is typically encoded in HTML format.
+    # We should get rid of the HTML tags and retrieve the text. How can we do it?
+
+    # Problem 2: Let's assume that we found a way to retrieve the text from a webpage. How can we recognize the
+    # entities in the text?
+
+    # Problem 3: We now have to disambiguate the entities in the text. For instance, let's assugme that we identified
+    # the entity "Michael Jordan". Which entity in Wikidata is the one that is referred to in the text?
+
+    # To tackle this problem, you have access to two tools that can be useful. The first is a SPARQL engine (Trident)
+    # with a local copy of Wikidata. The file "test_sparql.py" shows how you can execute SPARQL queries to retrieve
+    # valuable knowledge. Please be aware that a SPARQL engine is not the best tool in case you want to lookup for
+    # some strings. For this task, you can use elasticsearch, which is also installed in the docker image.
+    # The file start_elasticsearch_server.sh will start the elasticsearch server while the file
+    # test_elasticsearch_server.py shows how you can query the engine.
+
+    # A simple implementation would be to first query elasticsearch to retrieve all the entities with a label
+    # that is similar to the text found in the web page. Then, you can access the SPARQL engine to retrieve valuable
+    # knowledge that can help you to disambiguate the entity. For instance, if you know that the webpage refers to persons
+    # then you can query the knowledge base to filter out all the entities that are not persons...
+
+    # Obviously, more sophisticated implementations that the one suggested above are more than welcome :-)
+
+
+    # For now, we are cheating. We are going to returthe labels that we stored in sample-labels-cheat.txt
+    # Instead of doing that, you should process the text to identify the entities. Your implementation should return
+    # the discovered disambiguated entities with the same format so that I can check the performance of your program.
+    cheats = dict((line.split('\t', 2) for line in open('data/sample-labels-cheat.txt').read().splitlines()))
+    for label, wikidata_id in cheats.items():
+        if key and (label in payload):
+            yield key, label, wikidata_id
+
+
+def split_records(stream):
+    payload = ''
+    for line in stream:
+        if line.strip() == "WARC/1.0":
+            yield payload
+            payload = ''
+        else:
+            payload += line
+    yield payload
+
+if __name__ == '__main__':
+    import sys
+    try:
+        _, INPUT = sys.argv
+    except Exception as e:
+        print('Usage: python starter-code.py INPUT')
+        sys.exit(0)
+
+    with gzip.open(INPUT, 'rt', errors='ignore') as fo:
+        for record in split_records(fo):
+            for key, label, wikidata_id in find_labels(record):
+                print(key + '\t' + label + '\t' + wikidata_id)
