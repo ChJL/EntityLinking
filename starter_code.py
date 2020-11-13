8c6b471032919a35e0ab46947335900ab2125030
Jaak Lin <linjaak@gmail.com>
2020-11-13 15:58:23 +0100

    modify and clear

--- starter_code.py
+++ starter_code.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from bs4.element import Comment
 from warcio.archiveiterator import ArchiveIterator
 # https://pypi.org/project/warcio/
-from html_to_text import html_to_text, parse_html
+from html_to_text import html_to_text
 from NLP_utils import NLProcess, get_NER
 from test_elasticsearch_server import ELSsearch, search
 from strsimpy.cosine import Cosine
@@ -47,73 +47,65 @@ if __name__ == '__main__':
     except Exception as e:
         print('Usage: python starter-code.py INPUT(war.gz file)')
         sys.exit(0)
-    count = 0
+    
     with gzip.open(INPUT, 'rb') as stream:
         for record in ArchiveIterator(stream):
             if record.rec_type == 'response':
-                    #print(record.rec_headers.get_header(KEYNAME))
-                    # the key_ID storing WebpageID, the text storing the text converted by html
-                    key_ID = record.rec_headers.get_header(KEYNAME)
+                
+                # the key_ID storing WebpageID, the text storing the text converted by html
+                key_ID = record.rec_headers.get_header(KEYNAME)
 
-                    # try for few pages:
-                    #if key_ID == "clueweb12-0000tw-00-00017":
-                    #    break
+                # try for few pages:
+                #if key_ID == "clueweb12-0000tw-00-00017":
+                #    break
 
-                    htmlcontent = record.content_stream().read()
-                    
-                    # method for html to text, if the soup return is none, drop current webpage
-                    soup = BeautifulSoup(htmlcontent, "lxml")
-                    if soup == None:
+                htmlcontent = record.content_stream().read()
+                
+                # method for html to text, if the soup return is none, drop current webpage
+                soup = BeautifulSoup(htmlcontent, "lxml")
+                if soup == None:
+                    continue
+
+                # if there is no raw text return, we drop the current webpage
+                text = html_to_text(soup)
+                if text == "" or text == " XML RPC server accepts POST requests only ":
+                    continue
+
+                # The NER_mentions is a list with ("string","type")
+                NER_mentions = NLProcess(text)
+                # drop duplicate in NER_mentions
+                NER_mentions = list(dict.fromkeys(NER_mentions))
+
+                
+                final_entities = []
+                for mention in NER_mentions:
+                    # candidates is a dictionary with 10 results
+                    candidates = generate_candidates(mention[0])
+
+                    max_score = 0
+                    max_entity = []
+                    if candidates == None:
                         continue
-
-                    # if there is no raw text return, we drop the current webpage
-                    text = html_to_text(soup)
-                    if text == "" or text == " XML RPC server accepts POST requests only ":
-                        continue
-
-                    # The NER_mentions is a list with ("string","type")
-                    NER_mentions = NLProcess(text)
-                    # drop duplicate in NER_mentions
-                    NER_mentions = list(dict.fromkeys(NER_mentions))
-
-                    
-                    final_entities = []
-                    for mention in NER_mentions:
-                        # candidates is a dictionary with 10 results
-                        candidates = generate_candidates(mention[0])
-                        #print("mention: ",mention)
-                        #print("mention type: ", type(mention[0]))
-
-                        can_with_max_score = ""
-                        max_score = 0
-                        max_entity = []
-                        if candidates == None:
-                            continue
-                        for entity_id, labels in candidates:
-
-                            # convert labels into string type 
-                            labels_str = ', '.join(labels)
-                            
-                            
-                            # if words of mention are "completely" in labels, add a bonus score 0.1
-                            contain_score = 0
-                            if mention[0] in labels_str:
-                                contain_score = 0.1
-                            # do the string similarity
-                            temp_score = jaccard_sim(labels_str,mention[0]) + contain_score
-                            if temp_score > max_score:
-                                max_score = temp_score
-                                max_entity = [mention[0],entity_id]
-                            #print("temp_score: ", temp_score)
-                        if len(max_entity) != 0 and max_score >= 0.7:
-                            final_entities.append(max_entity)
+                    for entity_id, labels in candidates:
+                        # convert labels into string type 
+                        labels_str = ', '.join(labels)
                         
-
-                    #print("--final_entities--")
-                    if len(final_entities) > 0:
-                        #print(final_entities)
-                        for final_enity in final_entities:
-                            print(key_ID + '\t' + final_enity[0] + '\t' + final_enity[1])
+                        # if words of mention are "completely" in labels, add a bonus score 0.1
+                        contain_score = 0
+                        if mention[0] in labels_str:
+                            contain_score = 0.1
+                        # do the string similarity
+                        temp_score = jaccard_sim(labels_str,mention[0]) + contain_score
+                        if temp_score > max_score:
+                            max_score = temp_score
+                            max_entity = [mention[0],entity_id]
+                    # store the entity, only if the maxscore larger than 0.7 and max_entity exist
+                    if len(max_entity) != 0 and max_score >= 0.7:
+                        final_entities.append(max_entity)
+                    
+                if len(final_entities) > 0:
+                    for final_enity in final_entities:
+                        print(key_ID + '\t' + final_enity[0] + '\t' + final_enity[1])
 
 
         
