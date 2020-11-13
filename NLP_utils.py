1429b01855293f1a4c7a7ea48be0983e4a73c62d
Jaak Lin <linjaak@gmail.com>
2020-11-13 18:19:22 +0100

    add prediction for all sample.war with thres >=0.7

--- NLP_utils.py
+++ NLP_utils.py
@@ -1,11 +1,11 @@
 import nltk
-'''
+
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
-'''
+
 from nltk.tokenize import RegexpTokenizer
 from nltk.tokenize import word_tokenize, sent_tokenize
 
